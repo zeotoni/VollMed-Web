78ae85b5a587156134f325262ab6f450d287060e
zeotoni <zeotoni666@gmail.com>
2023-11-30 20:24:14 -0300

    feat: add button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './shared/container/container.component';
+import { ButtonComponent } from './shared/button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, ContainerComponent],
+  declarations: [AppComponent, ContainerComponent, ButtonComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {
+  @Input() text!: string;
+  @Input() style: 'primary' | 'secondary' = 'primary';
+}
