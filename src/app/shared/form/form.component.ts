a491ad9fe49918476d631303be20212a51f69071
zeotoni <zeotoni666@gmail.com>
2023-11-30 20:59:30 -0300

    feat: add form component

---
+++ src/app/shared/form/form.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent {
+  @Input() btnText!: string;
+
+  states = [
+    'AC',
+    'AL',
+    'AP',
+    'AM',
+    'BA',
+    'CE',
+    'DF',
+    'ES',
+    'GO',
+    'MA',
+    'MT',
+    'MS',
+    'MG',
+    'PA',
+    'PB',
+    'PR',
+    'PE',
+    'PI',
+    'RJ',
+    'RN',
+    'RS',
+    'RO',
+    'RR',
+    'SC',
+    'SP',
+    'SE',
+    'TO',
+  ];
+
+  specialties = [
+    'Ortopedista',
+    'Cardiologista',
+    'Ginecologista',
+    'Dermatologista',
+  ];
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
 import { ContainerComponent } from './container/container.component';
 import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [ButtonComponent, ContainerComponent, HeaderComponent],
+  declarations: [ButtonComponent, ContainerComponent, HeaderComponent, FormComponent],
   imports: [CommonModule],
   exports: [ButtonComponent, ContainerComponent, HeaderComponent],
 })
