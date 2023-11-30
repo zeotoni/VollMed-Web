1be3865655f9fe72b50aedbc6d34d81c25d5762e
zeotoni <zeotoni666@gmail.com>
2023-11-30 20:16:48 -0300

    chore: add container component

---
+++ src/app/shared/container/container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss'],
+})
+export class ContainerComponent {}
