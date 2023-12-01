45d03fbf7d942052e01120be7648f2cefd83dcb2
zeotoni <zeotoni666@gmail.com>
2023-12-01 18:09:30 -0300

    chore: exclude app.spec file && updating app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
 import { ButtonComponent } from './button/button.component';
 import { ContainerComponent } from './container/container.component';
-import { HeaderComponent } from './header/header.component';
 import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [ButtonComponent, ContainerComponent, HeaderComponent, FormComponent],
+  declarations: [
+    ButtonComponent,
+    ContainerComponent,
+    HeaderComponent,
+    FormComponent,
+  ],
   imports: [CommonModule],
-  exports: [ButtonComponent, ContainerComponent, HeaderComponent],
+  exports: [
+    ButtonComponent,
+    ContainerComponent,
+    HeaderComponent,
+    FormComponent,
+  ],
 })
 export class SharedModule {}
