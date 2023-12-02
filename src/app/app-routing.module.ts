7452949e5c77c0bd1f5996831a3dc7f0c019ce25
zeotoni <zeotoni666@gmail.com>
2023-12-02 09:50:52 -0300

    feat: add edit component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditComponent } from './pages/edit/edit.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'editar',
+    component: EditComponent,
   },
 ];
 
