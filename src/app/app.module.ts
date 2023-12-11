76deb3719a8e2c53b91f5288a3bf58766380d5a6
zeotoni <zeotoni666@gmail.com>
2023-12-11 13:29:36 -0300

    feat: add register function && update types

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { HomeComponent } from './pages/home/home.component';
 import { EditComponent } from './pages/edit/edit.component';
+import { HomeComponent } from './pages/home/home.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, EditComponent, SignUpComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    SharedModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Especialidade } from 'app/core/enums/especialidade';
+import { Estado } from 'app/core/enums/estados';
+import { Medico } from 'app/core/interfaces/medico';
+import { MedicosService } from './../../core/services/medicos.service';
 
 @Component({
   selector: 'app-form',
@@ -11,61 +15,61 @@ export class FormComponent implements OnInit {
 
   signUpForm!: FormGroup;
 
-  states = [
-    'AC',
-    'AL',
-    'AP',
-    'AM',
-    'BA',
-    'CE',
-    'DF',
-    'ES',
-    'GO',
-    'MA',
-    'MT',
-    'MS',
-    'MG',
-    'PA',
-    'PB',
-    'PR',
-    'PE',
-    'PI',
-    'RJ',
-    'RN',
-    'RS',
-    'RO',
-    'RR',
-    'SC',
-    'SP',
-    'SE',
-    'TO',
-  ];
+  states = Object.values(Estado).filter((value) => typeof value === 'string');
 
-  specialties = [
-    'Ortopedista',
-    'Cardiologista',
-    'Ginecologista',
-    'Dermatologista',
-  ];
+  specialties = Object.values(Especialidade).filter(
+    (value) => typeof value === 'string',
+  );
+
+  constructor(
+    private medicosService: MedicosService,
+    private formBuilder: FormBuilder,
+  ) {}
 
   ngOnInit(): void {
-    this.signUpForm = new FormBuilder().group({
+    this.signUpForm = this.formBuilder.group({
       nome: ['', [Validators.required]],
       especialidade: ['', [Validators.required]],
       crm: ['', [Validators.required, Validators.pattern(/^\d{4,6}$/)]],
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, Validators.maxLength(11)]],
-      rua: ['', [Validators.required]],
+      telefone: ['', [Validators.required, Validators.maxLength(11)]],
+      logradouro: ['', [Validators.required]],
       numero: [''],
       complemento: [''],
       cidade: ['', [Validators.required]],
-      estado: ['', [Validators.required]],
+      uf: ['', [Validators.required]],
       cep: ['', [Validators.required, Validators.pattern(/^\d{8}$/)]],
     });
   }
 
+  private extractFormValues(form: FormGroup): Medico {
+    return {
+      nome: form.get('nome')?.value,
+      especialidade: form.get('especialidade')?.value,
+      crm: form.get('crm')?.value,
+      email: form.get('email')?.value,
+      telefone: form.get('telefone')?.value,
+      endereco: {
+        logradouro: form.get('logradouro')?.value,
+        numero: form.get('numero')?.value,
+        complemento: form.get('complemento')?.value,
+        cidade: form.get('cidade')?.value,
+        uf: form.get('uf')?.value,
+        cep: form.get('cep')?.value,
+      },
+    };
+  }
+
   dataSignUp() {
-    console.log(this.signUpForm.value);
+    const novoMedico: Medico = this.extractFormValues(this.signUpForm);
+    this.medicosService.register(novoMedico).subscribe({
+      next: (value) => {
+        console.log(value);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
   btnState() {
