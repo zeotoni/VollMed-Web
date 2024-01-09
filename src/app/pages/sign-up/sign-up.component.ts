1a6f08e0822c8d90c8f1e621ab9bde2aed6f6649
zeotoni <zeotoni666@gmail.com>
2024-01-09 17:07:19 -0300

    feat: add message component to signup component && add navigation to list component

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Medico } from 'app/core/interfaces/medico';
+import { MedicosService } from 'app/core/services/medicos.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  modalText = 'Cadastro realizado com sucesso!';
+  @ViewChild('modal') modalWarning!: ElementRef;
+  constructor(
+    private medicosService: MedicosService,
+    private router: Router,
+  ) {}
+
+  onSubmit(data: Medico) {
+    this.medicosService.register(data).subscribe({
+      next: () => {
+        this.openModal();
+        setTimeout(() => {
+          this.closeModal();
+          this.router.navigate(['listar']);
+        }, 3000);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  openModal(): void {
+    this.modalWarning.nativeElement.firstChild.showModal();
+  }
+
+  closeModal(): void {
+    this.modalWarning.nativeElement.firstChild.close();
+  }
+}
