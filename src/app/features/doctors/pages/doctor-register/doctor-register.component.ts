a5b49a3eca89e0d3c2239e743c15d66e7ed63558
zeotoni <zeotoni666@gmail.com>
2024-09-24 21:59:31 -0300

    refactor: update doctor-register method to use cache structure

--- src/app/features/doctors/pages/doctor-register/doctor-register.component.ts
+++ src/app/features/doctors/pages/doctor-register/doctor-register.component.ts
@@ -1,8 +1,10 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
 import { Doctor } from '../../models/doctor';
 import { Specialty } from '../../models/specialty';
+import { DoctorListingService } from '../../services/doctor-listing.service';
 import { DoctorService } from '../../services/doctor.service';
 
 @Component({
@@ -20,6 +22,7 @@ export class DoctorRegisterComponent {
 
   constructor(
     private doctorService: DoctorService,
+    private doctorListingService: DoctorListingService,
     private router: Router,
   ) {}
 
@@ -35,20 +38,26 @@ export class DoctorRegisterComponent {
   onSubmit() {
     const data = this.form.value as unknown as Doctor;
 
-    this.doctorService.registerDoctor(data).subscribe({
-      next: () => {
-        this.openModal();
-        setTimeout(() => {
-          this.closeModal();
-          this.router.navigate(['doctors']);
-        }, 3000);
-      },
-      error: (error) => {
-        this.modalText = 'Não foi possível realizar o cadastro!';
-        this.openModal();
-        console.log('Erro ao cadastrar', error);
-      },
-    });
+    this.doctorService
+      .registerDoctor(data)
+      .pipe(
+        switchMap(() => this.doctorService.getDoctorList()),
+        tap((doctors) => this.doctorListingService.setUpdatedList(doctors)),
+      )
+      .subscribe({
+        next: () => {
+          this.openModal();
+          setTimeout(() => {
+            this.closeModal();
+          }, 3000);
+          this.form.reset({ specialty: '' });
+        },
+        error: (error) => {
+          this.modalText = 'Não foi possível realizar o cadastro!';
+          this.openModal();
+          console.log('Erro ao cadastrar', error);
+        },
+      });
   }
 
   openModal(): void {
